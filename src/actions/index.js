3257ef91c813b82b83ed8c99c683a7394dedc32f
Nemanja Sreckovic <nemanja.sreckovic@gmail.com>
2017-10-06 12:31:44 +1100

    Book detail
    Book detail

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,7 @@
 export function selectBook(book) {
-  console.log('A book has been selected:', book.title);
+  // selectBook is an ActionCreator, it needs to return an action, an object with type property.
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  };
 }
